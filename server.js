25a72f47d440a44c8dfbb079b86b6a30205ebb96
Raghuram42 <voruganti.raghuram@gmail.com>
2016-05-06 22:27:30 +0530

    Initilize repo's

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(middleware.logger);
 app.get('/aboutus',function(req,res){
    res.send("I'm cool and smart	");
 });
+
+app.get('/login',function(req,res){
+   res.send("Wait it's being designing!");
+});
 app.use(express.static(__dirname+ '/public'));
 
 app.listen(port,function(){
